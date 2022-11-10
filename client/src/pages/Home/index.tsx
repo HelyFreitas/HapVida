38ca25f94dcad56acec4541f75d5c0e5f2f5a6fa
Helyel <helyelestagio18@gmail.com>
2022-11-09 22:36:15 -0300

    added styles

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,9 +1,12 @@
 import * as C from "./styles";
 import Logo1 from "../../assets/logo1.png";
 import LogoContent1 from "../../assets/logo-content1.png";
-import Circle from "../../assets/circle.png"
+import Circle from "../../assets/circle.png";
+import CircleBlue from "../../assets/circleBlue.png";
+import ContactPng from "../../assets/User.png"
+import EmployerPng from "../../assets/Employer.png"
 
-import { ArrowUpRight } from "phosphor-react";
+import { ArrowCircleRight, ArrowUpRight } from "phosphor-react";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
@@ -16,49 +19,87 @@ export const Home = () => {
         </div>
         <div className="top-menu-content2">
           <img src={Logo1} alt="" className="top-container-logo" />
-          <ul>
-              <a className="top-menu-a" href="#">Institucional</a>
-              <a className="top-menu-a" href="#">
-                Investidores <ArrowUpRight size={14} />
-              </a>
-              <a className="top-menu-a" href="#">Rede de atendimento</a>
-            <select value="">
-              <option value="beneficiaries">Acesse sua área</option>
-              <option value="beneficiaries">
-                <Link to="/benefiaries">Beneficiários</Link>
-              </option>
-              <option value="beneficiaries">
-                <Link to="/brokers">Corretores</Link>
-              </option>
-              <option value="beneficiaries">
-                <Link to="/providers">Prestadores</Link>
-              </option>
-              <option value="beneficiaries">
-                <Link to="/client">Quero ser cliente</Link>
-              </option>
-            </select>
-          </ul>
-          <button>Conhecer Planos</button>
+          <div className="itens-nav">
+            <a className="top-menu-nav" href="#">
+              Institucional
+            </a>
+            <a className="top-menu-nav" href="#">
+              Investidores <ArrowUpRight size={14} />
+            </a>
+            <a className="top-menu-nav" href="#">
+              Rede de atendimento
+            </a>
+            <p className="top-menu-nav">Acesse sua área</p>
+          </div>
+          <button className="top-menu-btn">Conhecer Planos</button>
         </div>
       </section>
 
       {/*//////////////////////////////////////////////////////////////////////////*/}
 
       <C.Header>
-        <h1>Somos Hapvida & NotreDame Intermédica</h1>
-        <p className="header-p">
-          A maior rede própria de atendimento do Brasil.
-        </p>
-        <img src={Logo1} className="header-img" />
-        <button>Sobre a Fusão Hapvida e GNDI</button>
-        <div className="circles">
-          <img src={Circle} alt="" />
-          <img src={Circle} alt="" />
-          <img src={Circle} alt="" />
-          <img src={Circle} alt="" />
-          <img src={Circle} alt="" />
+        <div className="divdiv">
+          <h1 className="header-title">
+            Somos
+            <br /> Hapvida &<br /> NotreDame Intermédica
+          </h1>
+          <p className="header-p">
+            A maior rede própria de atendimento do Brasil.
+          </p>
+          <img src={Logo1} className="header-img" />
+          <button className="header-btn">Sobre a Fusão Hapvida e GNDI</button>
+          <div className="circles">
+            <img src={CircleBlue} alt="" />
+            <img src={Circle} alt="" />
+            <img src={Circle} alt="" />
+            <img src={Circle} alt="" />
+            <img src={Circle} alt="" />
+          </div>
         </div>
       </C.Header>
+      <hr className="divisor" />
+      <C.Main>
+        <C.FirstContent>
+          <section className="itens-main">
+            <div className="title-tt">
+              <h1 className="firstcontent-title">
+                Hapvida e NotreDame Intermédica:
+                <br /> dois gigantes da saúde juntos
+              </h1>
+            </div>
+            <div className="card-plan">
+              <p className="card-plan-title">
+                Mais saúde de qualidade.
+                <br /> Mais perto de você.
+              </p>
+              <button className="card-plan-btn">Conhecer planos</button>
+            </div>
+          </section>
+
+          <div className="paragraph-main">
+            <h1 className="paragraph-main-title">Acesse sua área</h1>
+            <p className="paragraph-main-p">
+              Espaço personalizado de acordo com o seu perfil.
+            </p>
+          </div>
+
+          <section className="cards-itens-main">
+            <div className="card1">
+              <img className="card-img" src={ContactPng} alt="" />
+              <h1 className="card-title">Seja Cliente</h1>
+              <p className="card-p">Conheça nossos planos e faça sua cotação.</p>
+              <ArrowCircleRight className="icon-card" size={28} color="#0054B8" weight="fill" />
+            </div>
+            <div className="card2">
+              <img className="card-img" src={EmployerPng} alt="" />
+              <h1 className="card-title">Empresas</h1>
+              <p className="card-p">Consulte serviços e mais.</p>
+              <ArrowCircleRight className="icon-card" size={28} color="#0054B8" weight="fill" />
+            </div>
+          </section>
+        </C.FirstContent>
+        <hr className="divisor2" />
+      </C.Main>
     </C.Container>
   );
 };
--- client/src/pages/Home/styles.ts
+++ client/src/pages/Home/styles.ts
@@ -1,7 +1,10 @@
 import styled from "styled-components";
+import Back from "../../assets/back.png";
 
 export const Container = styled.div`
   .top-menu {
+    border-bottom: 1px solid #ccccd0;
+
     .top-menu-content1 {
       display: flex;
 
@@ -28,17 +31,327 @@ export const Container = styled.div`
       }
     }
 
-    .top-menu-content2{
+    .top-menu-content2 {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .itens-nav {
         display: flex;
-        align-items: center;
+      }
 
-        .top-menu-a{
-            text-decoration: none;
+      .top-menu-nav {
+        text-decoration: none;
 
-            padding-right: 80px;
-        }
+        padding-right: 50px;
+
+        font-family: "Source Sans Pro";
+        font-style: normal;
+        font-weight: 600;
+        font-size: 16px;
+        line-height: 20px;
+
+        color: #58585a;
+      }
+
+      .top-container-logo {
+        padding-right: 40px;
+
+        padding-top: 5px;
+        padding-bottom: 5px;
+      }
+    }
+
+    .top-menu-btn {
+      background: #0054b8;
+      border-radius: 40px;
+
+      padding: 12px;
+
+      border: none;
+
+      font-weight: 600;
+
+      color: white;
+
+      margin-left: 40px;
+    }
+  }
+
+  .divisor {
+    width: 100%;
+    height: 7px;
+
+    background-color: #f49e00;
+  }
+`;
+
+export const Header = styled.header`
+  background-image: url(${Back});
+  background-size: cover;
+
+  height: 480px;
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: left;
+
+  position: relative;
+
+  .divdiv {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+
+    margin-left: 145px;
+  }
+
+  .header-title {
+    font-family: "Source Sans Pro";
+    font-style: normal;
+    font-weight: 900;
+    font-size: 48px;
+    line-height: 60px;
+    letter-spacing: 0.08px;
+
+    color: #0054b8;
+
+    padding-bottom: 20px;
+  }
+
+  .header-p {
+    font-family: "Source Sans Pro";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 30px;
+    letter-spacing: 0.08px;
+
+    /* Neutral / Gray 1 */
+
+    color: #5b5b5f;
+
+    padding-bottom: 24px;
+  }
+
+  .header-img {
+    width: 328px;
+    height: 60px;
+
+    margin-bottom: 24px;
+  }
+
+  .header-btn {
+    font-family: "Source Sans Pro";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 25px;
+    text-align: center;
+
+    color: #ffffff;
+
+    background: #0054b8;
+    border-radius: 40px;
+
+    width: 328px;
+
+    padding-bottom: 16px;
+    padding-top: 16px;
+
+    border: none;
+  }
+
+  .circles {
+    position: absolute;
+
+    top: 448px;
+    left: 720px;
+  }
+`;
+
+export const Main = styled.main`
+  .divisor2 {
+    width: 100%;
+    height: 7px;
+
+    background-color: #0054b8;
+
+    margin-bottom: 100px;
+  }
+`;
+
+export const FirstContent = styled.div`
+  .itens-main {
+    display: flex;
+    justify-content: center;
+
+    margin-top: 60px;
+
+    .firstcontent-title {
+      font-family: "Source Sans Pro";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 40px;
+      line-height: 50px;
+      letter-spacing: 0.08px;
+
+      color: #0054b8;
+
+      padding-right: 82px;
+    }
+
+    .card-plan {
+      background: #db6e2d;
+      border-radius: 20px;
+
+      padding: 33px;
+
+      display: flex;
+      flex-direction: column;
+
+      .card-plan-title {
+        font-family: "Source Sans Pro";
+        font-style: normal;
+        font-weight: 700;
+        font-size: 24px;
+        line-height: 30px;
+        text-align: center;
+        letter-spacing: 0.08px;
+
+        /* Neutral / White */
+
+        color: #ffffff;
+
+        padding-bottom: 25px;
+      }
+
+      .card-plan-btn {
+        width: 390px;
+
+        padding-top: 17px;
+        padding-bottom: 17px;
+        margin-bottom: 6px;
+
+        background: #ffffff;
+        border-radius: 40px;
+
+        font-family: "Source Sans Pro";
+        font-style: normal;
+        font-weight: 700;
+        font-size: 24px;
+        line-height: 30px;
+
+        /* Laranja Hap */
+
+        color: #db6e2d;
+
+        border: none;
+      }
+    }
+  }
+
+  .paragraph-main {
+    margin-left: 380px;
+    margin-top: 41px;
+
+    .paragraph-main-title {
+      font-family: "Source Sans Pro";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 36px;
+      line-height: 117.2%;
+      /* or 42px */
+
+      letter-spacing: 0.58px;
+
+      /* Azul Hapvida */
+
+      color: #0054b8;
+    }
+
+    .paragraph-main-p {
+      font-family: "Source Sans Pro";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 24px;
+      line-height: 30px;
+      letter-spacing: 0.08px;
+
+      /* Cinza GNDI */
+
+      color: #58585a;
+
+      margin-bottom: 27px;
+    }
+  }
+
+  .cards-itens-main {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 375px;
+    padding-right: 375px;
+    margin-bottom: 65px;
+
+    .card1,
+    .card2 {
+      background: #ffffff;
+      border: 1px solid #c0c0c0;
+      border-left: 10px solid #0054b8;
+      border-radius: 10px;
+
+      width: 554px;
+
+      justify-content: center;
+
+      position: relative;
+
+      padding-left: 20px;
+
+      .card-img {
+        padding-top: 25px;
+      }
+
+      .card-title {
+        font-family: "Source Sans Pro";
+        font-style: normal;
+        font-weight: 900;
+        font-size: 40px;
+        line-height: 50px;
+        letter-spacing: 0.08px;
+
+        /* Branding/Hapvida_Blue */
+
+        color: #0054b8;
+      }
+
+      .card-p {
+        font-family: "Source Sans Pro";
+        font-style: normal;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 20px;
+
+        /* Branding/Hapvida_Blue */
+
+        color: #0054b8;
+
+        padding-bottom: 20px;
+      }
+
+      .icon-card {
+        position: absolute;
+        top: 60px;
+        right: 10px;
+      }
     }
   }
 `;
 
-export const Header = styled.header``;
+export const SecondContent = styled.div``;
+
+export const ThirdContent = styled.div``;
+
+export const BedroomContent = styled.div``;
