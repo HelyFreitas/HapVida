67ba88074b8d6ab6332f2994d1ac03962960e149
Helyel <helyelestagio18@gmail.com>
2022-11-13 01:17:46 -0300

    added new routes

---
+++ client/src/pages/Broker/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import * as C from "./styles";
+
+export const Broker = () => {
+    return(
+        <C.Container>
+            <h1>Testes</h1>
+            <Link to="/">Sair</Link>
+        </C.Container>
+    )
+}
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Home } from "../pages/Home";
 import { AboutInstitutional } from "../pages/AboutInstitutional";
 import { ServiceNetworks } from "../pages/ServiceNetworks";
+import { Providers } from "../pages/Providers";
+import { Plans } from "../pages/Plans";
+import { Broker } from "../pages/Broker";
+import { Beneficiaries } from "../pages/Beneficiaries";
 
 export const Rotas = () => {
     return(
@@ -10,6 +14,10 @@ export const Rotas = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/about-institutional" element={<AboutInstitutional />} />
                 <Route path="/service-networks" element={<ServiceNetworks />} />
+                <Route path="/beneficiaries" element={<Beneficiaries />} />
+                <Route path="/broker" element={<Broker />} />
+                <Route path="/plans" element={<Plans />} />
+                <Route path="/providers" element={<Providers />} />
             </Routes>
         </Router>
     );
